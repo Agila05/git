96f8161ab682a1ea937fc4049219f818eaf0036c
Agila05 <agigunaa05@gmail.com>
2019-09-19 06:27:21 -0700

    I am adding my code

---
+++ src/git/Gitsample.java
@@ -0,0 +1,18 @@
+package git;
+
+public class Gitsample {
+	public static void main(String args[])
+	{
+		     int i;
+	         	String a[]=new String[4];
+	          	a[0]="java";
+	         	a[1]="agi";
+		        a[2]="guna";
+		        a[3]="kk";
+		       // System.out.println(a.length);
+		        for(i=0;i<a.length;i++)
+		        {
+		        	System.out.println(a[i]);
+		        }
+	}
+}
